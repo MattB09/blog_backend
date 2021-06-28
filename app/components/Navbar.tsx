3bc039712ff2bb7bd3df8ad6a735bd0a94a052a5
MattB09 <mattbancroft03@gmail.com>
2021-06-28 14:26:01 -0400

    Nav logic.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,32 +1,41 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 
 export default function Navbar() {
   const [status, setStatus] = useState('unauthenticated')
-  const [navDisplay, setNavDisplay] = useState('false');
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [navDisplay, setNavDisplay] = useState(false);
 
-  // add useEffect to automatically turn nav to true when the window size is increased.
+  useEffect(()=> {
+    setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', () => {
+      setWindowWidth(window.innerWidth);
+    });
+    if (windowWidth > 767) setNavDisplay(true);
+    else setNavDisplay(false);
+  }, [windowWidth])
+
   function toggleNav() {
-    console.log("clicked");
+    if (windowWidth < 768) setNavDisplay(prev => !prev);
   }
 
   return (
-    <header className="flex items-center justify-between bg-blue-800 text-gray-100 py-3 px-6">
+    <header className="flex items-center flex-wrap bg-blue-800 text-gray-100 py-3 px-6">
 
       <Link href="/">
-        <a className="no-underline font-bold p-2">
+        <a className="no-underline font-bold">
           Blog
         </a>
       </Link>
 
-      <button className="md:hidden items-end">
+      <button className="inline-flex px-2 py-1 hover:bg-gray-100 hover:text-blue-800 rounded md:hidden ml-auto outline-none focus:outline-none" onClick={toggleNav}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
 
-      <nav className="items-start">
-        <ul className="hidden top-navbar w-full md:inline-flex md:flex-grow md:w-auto ">
+      <nav className={`${!navDisplay && 'hidden'} ml-auto w-full md:w-auto`}>
+        <ul className="w-full items-start justify-end flex flex-col md:inline-flex md:flex-row md:w-auto md:ml-auto md:items-center md:h-auto">
           <li className="mr-3"><Link href="/"><a onClick={toggleNav}>Home</a></Link></li>
           {status === 'authenticated' 
           ? (
@@ -46,7 +55,7 @@ export default function Navbar() {
             <li className="">
               <Link href="/signin">
                 <a onClick={toggleNav} className="">
-                  Get Started
+                  Sign In
                 </a>
               </Link>
             </li>
