970bdebe299277a6780c0e36f3dc8c124d2fd3f3
MattB09 <mattbancroft03@gmail.com>
2021-07-04 19:14:32 -0400

    Created getUserStories endpoint.

--- api/server/controller.ts
+++ api/server/controller.ts
@@ -1,14 +1,14 @@
 import pool from './db/index'
 import { Request, response, Response} from 'express'
 
-const DEFAULT_LIMIT = 12
+const DEFAULT_LIMIT: number = 12
 
 const getStories = async (req: Request, res: Response):Promise<Response> => {
   let page: number = Number(req.query.page) || 1
   let limit: number = Number(req.query.limit) || DEFAULT_LIMIT
   let offset: number = (page - 1) * limit
   let total = await pool.query('SELECT count(*) FROM stories')
-  let result = await pool.query('SELECT stories.*, users.email, users.avatar_color FROM stories \ JOIN users ON stories.user_id = users.id ORDER BY date_added DESC \ LIMIT $1 OFFSET $2', [limit, offset])
+  let result = await pool.query('SELECT stories.*, users.email, users.avatar_color FROM stories JOIN users ON stories.user_id = users.id ORDER BY date_added DESC LIMIT $1 OFFSET $2', [limit, offset])
 
   let pages = Math.ceil(Number(total.rows[0].count) / limit)
 
@@ -26,7 +26,28 @@ const getStory = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json(result.rows[0]);
 }
 
+const getUserStories = async (req: Request, res: Response): Promise<Response> => {
+  const page: number = Number(req.query.page) || 1
+  const limit: number = Number(req.query.limit) || DEFAULT_LIMIT
+  const offset: number = (page - 1) * limit
+
+  const user = await pool.query('SELECT id FROM users WHERE id=$1', [req.params.id]);
+  if (user.rowCount === 0) return response.sendStatus(404)
+
+  let total = await pool.query('SELECT count(*) FROM stories WHERE user_id=$1', [req.params.id]);
+  let result = await pool.query('SELECT stories.*, users.email, users.avatar_color FROM stories JOIN users ON stories.user_id = users.id WHERE users.id=$1 ORDER BY date_added DESC LIMIT $2 OFFSET $3', [req.params.id, limit, offset])
+
+  let pages = Math.ceil(Number(total.rows[0].count) / limit);
+
+  return res.status(200).json({
+    total: {stories: total.rows[0].count, pages},
+    page,
+    rows: result.rows
+  })
+}
+
 export { 
   getStories, 
-  getStory 
+  getStory,
+  getUserStories,
 }
--- api/server/routes.ts
+++ api/server/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import { getStories, getStory } from './controller'
+import { getStories, getStory, getUserStories } from './controller'
 
 const routes = Router()
 
+//
 routes.get('/stories', getStories)
 
 routes.get('/stories/:id', getStory)
+
+routes.get('/users/:id', getUserStories)
 
 export default routes
