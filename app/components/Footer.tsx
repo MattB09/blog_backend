81e0e542b8a485230804c9e48a8a89449c73e05c
MattB09 <mattbancroft03@gmail.com>
2021-07-11 06:24:33 -0400

    Edited footer styling.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-center py-2 mt-8">
+    <footer className="text-center py-2 mt-8 text-gray-500 font-semibold">
       <p>Placeholder text for footer.</p>
     </footer>
   )
