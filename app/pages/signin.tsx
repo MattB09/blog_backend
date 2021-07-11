0406cb10b8ad5314c4fb6f01ded4c361ffb54787
MattB09 <mattbancroft03@gmail.com>
2021-07-11 06:43:27 -0400

    Added form for signup on the the signin page. Not finished with styling.

--- app/pages/signin.tsx
+++ app/pages/signin.tsx
@@ -5,8 +5,10 @@ import API from '../utils/api'
 
 const SignIn: React.FC = () => {
   const { status, login } = useAuthContext()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState<string>('')
+  const [signupEmail, setSignupEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('');
+  const [signupPassword, setSignupPassword] = useState<string>('')
   const router: NextRouter = useRouter()
 
   useEffect(() => {
@@ -15,7 +17,7 @@ const SignIn: React.FC = () => {
     }
   }, [status])
 
-  const handleSubmit = async (clickEvent:FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSignInSubmit = async (clickEvent:FormEvent<HTMLFormElement>): Promise<void> => {
     clickEvent.preventDefault()
 
     if (email === '' || password === '') {
@@ -25,15 +27,29 @@ const SignIn: React.FC = () => {
 
     const result = await API.post(`/login`, {email, password}, {withCredentials: true})
     login({ at: result.data.accessToken, expire: result.data.expire })
-    router.push('/');
+    router.push('/')
+  }
+
+  const handleSignupSubmit = async (clickEvent: FormEvent<HTMLFormElement>): Promise<void> => {
+    clickEvent.preventDefault()
+
+    if (signupEmail === '' || signupPassword === '') {
+      alert("must fill out form");
+      return;
+    }
+
+    alert(`signup clicked, ${signupEmail}, ${signupPassword}`)
+
+    router.push('/')
   }
 
   return (
-    <main className="flex flex-col justify-center px-4 mt-4 sm:w-96 mx-auto">
+    <main className="flex flex-wrap justify-center px-4 mt-4 mx-auto">
       {status == 'unauthenticated' && 
-      (<> 
+      (<>
+      <div className="w-full sm:w-96"> 
         <h1 className="font-bold text-4xl tracking-wide block">Sign In</h1>
-        <form onSubmit={handleSubmit} className="mt-4">
+        <form onSubmit={handleSignInSubmit} className="mt-4">
 
           <label htmlFor="email" className="block text-sm text-gray-600 mb-2">Email</label>
           <input id="email" value={email} type="email" placeholder="email" className="w-full px-4 py-1 rounded" 
@@ -46,6 +62,24 @@ const SignIn: React.FC = () => {
           
           <button type="submit" className="bg-blue-800 text-gray-100 py-1 rounded text-center w-full mt-8">Sign In</button>
         </form>
+      </div>
+
+      <div className="w-full sm:w-96">  
+        <h1 className="font-bold text-4xl tracking-wide block mt-16 lg:mt-4">New User? Sign up</h1>
+        <form onSubmit={handleSignupSubmit} className="mt-4">
+
+          <label htmlFor="signupEmail" className="block text-sm text-gray-600 mb-2">Email</label>
+          <input id="signupEmail" value={signupEmail} type="email" placeholder="email" className="w-full px-4 py-1 rounded" 
+              onChange={e => setSignupEmail(e.target.value)} />
+
+          <label htmlFor="signupPassword" className="block text-sm text-gray-600 mt-4 mb-2">Password</label>
+          <input id="signupPassword" value={signupPassword} type="password" placeholder="password" className="w-full px-4 py-1 rounded"
+            onChange={e => setSignupPassword(e.target.value)} />
+            
+          
+          <button type="submit" className="bg-blue-800 text-gray-100 py-1 rounded text-center w-full mt-8">Sign In</button>
+        </form>
+      </div>
       </>)}
     </main>
   )
