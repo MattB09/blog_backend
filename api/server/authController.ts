e0411708ad2ce3b97dfb1b566dca8545320e04a4
MattB09 <mattbancroft03@gmail.com>
2021-07-13 13:34:18 +0900

    Separated signup from login and added error messages as alerts accordingly.

--- api/server/authController.ts
+++ api/server/authController.ts
@@ -6,7 +6,7 @@ import { createAccessToken, createRefreshToken, accessTokenExpire, refreshTokenE
 import { Request, Response } from 'express'
 import { UserInfo } from '../types'
 
-const login = async (req: Request, res:Response): Promise<Response> => {
+export const login = async (req: Request, res:Response): Promise<Response> => {
   const email: string = req.body.email
   const password: string = req.body.password
   
@@ -20,7 +20,7 @@ const login = async (req: Request, res:Response): Promise<Response> => {
     // user is already registered. validate password and send token
     user = user.rows[0]
     try {
-      const compareRes = await bcrypt.compare(password, user.password_hash)
+      const compareRes: boolean = await bcrypt.compare(password, user.password_hash)
       if (compareRes) {
         let payload: UserInfo = {
           userId: user.id,
@@ -38,33 +38,50 @@ const login = async (req: Request, res:Response): Promise<Response> => {
           accessToken: createAccessToken(payload),
           expire: accessTokenExpire
         })
-      } else {
+      } else { // password is invalid
         return res.status(401).json({ error: 'Invalid password'})
       }
     } catch (err) {
       console.log(err)
       return res.status(401).json({error: 'Something went wrong while authenticating'})
     }
-  }
-  // user is not yet registered. Register and send token
-  try {
-    const password_hash = await bcrypt.hash(password, 10)
-    const user_color = colorGenerator()
-    user = await pool.query('INSERT INTO users (email, password_hash, avatar_color) VALUES ($1, $2, $3) RETURNING *', [email, password_hash, user_color])
-    user = user.rows[0]
-    let payload: UserInfo = {
-      userId: user.id,
-      email: user.email,
-      avatar: user.avatar_color
-    }
-    res.cookie('rt', createRefreshToken(payload), { expires: new Date(Date.now() + refreshTokenExpire), httpOnly: true })
-    return res.status(200).json({ accessToken: createAccessToken(payload), expire: accessTokenExpire })
-  } catch (err) {
-    return res.status(400).json({error: err})
+  } else {   // user doesn't exist 
+    return res.status(404).json({error: 'Email has not been registered yet.'})
   }
 }
 
-const refreshToken = async (req: Request, res: Response): Promise<Response> => {
+export const signup = async (req: Request, res: Response): Promise<Response> => {
+  const email: string = req.body.signupEmail
+  const password: string = req.body.signupPassword
+  
+  if (!email || !password) {
+    return res.json({ error: 'email and password required'})
+  }
+
+  let user = await pool.query('SELECT * FROM users WHERE LOWER(email)=$1', [email.toLowerCase()])
+
+  if (user.rowCount === 0) {
+    try {
+      const password_hash = await bcrypt.hash(password, 10)
+      const user_color = colorGenerator()
+      user = await pool.query('INSERT INTO users (email, password_hash, avatar_color) VALUES ($1, $2, $3) RETURNING *', [email, password_hash, user_color])
+      user = user.rows[0]
+      let payload: UserInfo = {
+        userId: user.id,
+        email: user.email,
+        avatar: user.avatar_color
+      }
+      res.cookie('rt', createRefreshToken(payload), { expires: new Date(Date.now() + refreshTokenExpire), httpOnly: true })
+      return res.status(200).json({ accessToken: createAccessToken(payload), expire: accessTokenExpire })
+    } catch (err) {
+      return res.status(400).json({error: err})
+    }
+  } else {
+    return res.status(400).json({error: 'A user has already been registered to that email address.'})
+  }
+}
+
+export const refreshToken = async (req: Request, res: Response): Promise<Response> => {
   const token = req.cookies.rt
 
   if (!token) return res.json({ ok: 'false', rt: '' })
@@ -88,16 +105,11 @@ const refreshToken = async (req: Request, res: Response): Promise<Response> => {
   return res.json({ok: 'true', accessToken: createAccessToken(user), expire: accessTokenExpire })
 }
 
-const logout = async (req: Request, res: Response): Promise<Response> => {
+export const logout = async (req: Request, res: Response): Promise<Response> => {
   res.cookie('rt', '', { httpOnly: true })
   return res.sendStatus(204)
 }
 
-export {
-  login, 
-  refreshToken, 
-  logout
-}
 
 function colorGenerator(): string {
   const letters = "0123456789ABCDEF";
