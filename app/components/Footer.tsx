520cd663bbb1bc525dfe6d953a93600f8d8e0180
MattB09 <mattbancroft03@gmail.com>
2021-07-15 21:44:58 +0900

    Added skeleton footer items

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,26 @@ import Link from 'next/link'
 const Footer: React.FC = () => {
   return (
     <footer className="text-center py-2 mt-8 text-gray-500 font-semibold">
-      <p>Placeholder text for footer.</p>
+      <div>
+        <Link href="/">
+          <a>Blog</a>
+        </Link>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li><address>(000) 000 - 0000</address></li>
+          <li><address>blogemail@blogemail.com</address></li>
+        </ul>
+      </div>
+      <div>
+        <h3>Company</h3>
+        <ul>
+          <li><Link href="/about"><a>About</a></Link></li>
+          <li>Careers</li>
+        </ul>
+      </div>
+      <p>Copyright 2021 Blog. All rights reserved.</p>
     </footer>
   )
 }
