8a5435dfe653d4b20c0dea4e565677dc3988a9aa
MattB09 <mattbancroft03@gmail.com>
2021-07-20 18:32:18 +0900

    Added production api endpoint.

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '';
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://ec2-52-198-41-189.ap-northeast-1.compute.amazonaws.com:4000';
 
 const instance = axios.create({
   baseURL: API_URL
